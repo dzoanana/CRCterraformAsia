544cba3024111f71cedbbf273b2dc4c517f8ce15
Asia Kaczmarek <alina16ka@gmail.com>
2026-04-29 18:19:17 +0200

    key vault access policy client config

--- main.tf
+++ main.tf
@@ -36,6 +36,23 @@ resource "azurerm_key_vault_access_policy" "current_user" {
   ]
 }
 
+resource "azurerm_key_vault_access_policy" "azurerm_client_config" {
+  key_vault_id = azurerm_key_vault.default.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Create", "Delete", "Get", "List", "Update",
+    "Purge", "Recover", "Decrypt", "Encrypt",
+    "Sign", "Verify", "WrapKey", "UnwrapKey",
+    "GetRotationPolicy", "SetRotationPolicy",
+  ]
+
+  secret_permissions = [
+    "Get", "List", "Set", "Delete", "Purge", "Recover",
+  ]
+}
+
 # -------------------------------------------------------
 # Etap 3: Klucz RSA
 # -------------------------------------------------------
